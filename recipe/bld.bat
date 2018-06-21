b2df67f43e36ba915f848384c9a4314a8d45f53b
Hartikainen Ari <ari.hartikainen@aalto.fi>
2018-06-21 10:27:05 +0300

    lc for bash

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,13 +1,24 @@
+IF "%ARCH%" == "64" (
+set GCC_ARCH=x86_64-w64-mingw32
+set EXTRA_FLAGS=-DMS_WIN64
+) else (
+set GCC_ARCH=i686-w64-mingw32
+)
+
+set CFLAGS="-I$PREFIX/include $CFLAGS"
+set CPPFLAGS="-I$PREFIX/include $CPPFLAGS"
+set LDFLAGS="-L$PREFIX/lib $LDFLAGS"
+
 # Patch (support old gcc)
-bash "sed -i.bak 's/-Wno-unused-result//g' Makefile"
+bash -lc "sed -i.bak 's/-Wno-unused-result//g' Makefile"
 
 # Build
-bash "make lib"
+bash -lc "make lib"
 
 # Test
-bash "make test"
+bash -lc "make test"
 
 # Install
-bash "mkdir -p \"${PREFIX}/lib\" \"${PREFIX}/include\""
-bash cp "libjbig/*.a \"${PREFIX}/lib\""
-bash cp "libjbig/*.h \"${PREFIX}/include\""
+bash -lc "mkdir -p \"${PREFIX}/lib\" \"${PREFIX}/include\""
+bash -lc "cp libjbig/*.a \"${PREFIX}/lib\""
+bash -lc "cp libjbig/*.h \"${PREFIX}/include\""
